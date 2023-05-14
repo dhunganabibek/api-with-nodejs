9b7672f3690e797eef08ed986cc841fe363fd981
Bibek Dhungana <Dhunganabibek2016@gmail.com>
2023-05-14 18:48:00 -0500

    query the database.

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+let words = {
+    "rainbow": 5,
+    "unicorn": 3,
+    "hello": 2,
+    "gloom": -1
+}
+
+
 console.log(`Node is running`);
 
 var express = require('express');
@@ -24,3 +32,53 @@ app.get('/search/:flower/:num', function(req, res){
     res.send(reply);
 })
 
+
+app.get('/all', (req, res) => {
+    res.send(words);
+})
+
+
+//add word to the json
+app.get('/add/:word/:score?', (req, res) => {
+    let data = req.params;
+    let word = data.word;
+    let score = Number(data.score);
+    let reply = {};
+
+    words[word] = score;
+
+    if(!score){
+        reply = {
+            msg: "score is  required",
+        }
+    }
+    else{
+        reply = {
+            msg:"Thank you so much for your word"
+        }
+    }
+
+    res.send(reply);
+})
+
+app.get('/search/:word', (req, res) => {
+    let word = req.params.word;
+    let reply;
+
+    if (words[word]){
+        reply = {
+            status:"found",
+            word: word,
+            score: words[word]
+        }
+    }
+    else{
+        reply = {
+            status: "Not Found",
+            word: word,
+        }
+    }
+
+    return res.send(reply);
+
+})
